5e245210a78907329188759d9ed707db8bd9a3cb
Sahdat hossen <sahdathossen76@gmail.com>
2021-06-20 12:48:41 +0600

    featured product

--- js/script.js
+++ js/script.js
@@ -34,3 +34,23 @@ function prev(){
   index = (index - 1 + slides.length) % slides.length;
   slides[index].classList.add('hero__active');
 }
+document.querySelectorAll('.img-1').forEach(image_1 =>{
+  image_1.addEventListener('click', () =>{
+    var src = image_1.getAttribute('src');
+    document.querySelector('.big-img-1').src = src;
+  });
+});
+
+document.querySelectorAll('.img-2').forEach(image_2 =>{
+  image_2.addEventListener('click', () =>{
+    var src = image_2.getAttribute('src');
+    document.querySelector('.big-img-2').src = src;
+  });
+});
+
+document.querySelectorAll('.img-3').forEach(image_3 =>{
+  image_3.addEventListener('click', () =>{
+    var src = image_3.getAttribute('src');
+    document.querySelector('.big-img-3').src = src;
+  });
+});
